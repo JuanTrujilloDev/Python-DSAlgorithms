f90e4f5c27987ff1de9b4b04f053711750c570bd
Juan David Trujillo Suarez <sarsu1104@gmail.com>
2022-03-01 17:08:36 -0500

    Hash table implementation

--- DataStructures/Hash Tables/implementation.py
+++ DataStructures/Hash Tables/implementation.py
@@ -1,4 +1,7 @@
 
+from ast import Raise
+
+
 class MyHash:
     
     
@@ -28,8 +31,11 @@ class MyHash:
         Para alocar el espacio del arreglo en el cual almacenar el valor
         
         '''
-        
-        self.data[self._hash(key)] = [key, value]
+        if not self.data[self._hash(key)]:
+            self.data[self._hash(key)] = [[key, value]]
+            
+        else:
+            self.data[self._hash(key)].append([key,value])
         
         
     def get(self, key):
@@ -39,14 +45,49 @@ class MyHash:
         Recibe una clave, esta la hashea a numero y busca en la posicion dentro del arreglo
         A partir de ese numero, al final retornara el valor en esa posicion
         '''
-        
-        return self.data[self._hash(key)][1]
+        if self.data[self._hash(key)]:
+            
+            if len(self.data[self._hash(key)]) == 1:
+                return self.data[self._hash(key)][1]
+            
+            else:
+                
+                for array in self.data[self._hash(key)]:
+                    if array[0] == key:
+                        
+                        return  array[1]
+                    
+        return KeyError('Key does not exist!')
     
+    
+    def keys(self):
+        '''
+        keys()
+        
+        Generates multiple keys for each key, value pair.
+        '''
+        
+        for bucket in self.data:
+            if bucket:
+                if len(bucket) > 1 :
+                    for array in bucket:
+                        yield array[0]
+                    
+                    continue
+                        
+                yield bucket[0][0]
     
 
-new_dict = MyHash(50)
+new_dict = MyHash(3)
 
 new_dict.set('Nombre', 'Juan David')
 new_dict.set('Edad', 23)
+new_dict.set('Genero', 'Masculino')
+new_dict.set('Cargo', 'Desarrollador Python')
+new_dict.set('Mascotas', True)
+print(new_dict.data)
 print(new_dict.get('Nombre'))
 print(new_dict.get('Edad'))
+
+for i in new_dict.keys():
+    print(i)
